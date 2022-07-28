c77ea29fdb1bc051e69e2df0d2a38b40011b0715
mfigurski80 <miko.f80@gmail.com>
2022-07-28 01:25:24 +0000

    Deploying to gh-pages from @ mfigurski80/ClearingHouse@2be631fecf39fbd26789ab78cb292fa3351d91d9 🚀

---
+++ js/about.77fa2dba.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclearing_house"]=self["webpackChunkclearing_house"]||[]).push([[443],{6953:function(n,e,c){c.r(e),c.d(e,{default:function(){return l}});var r=c(3396);const s={class:"connect"},t=(0,r._)("h1",null,"This is the Connect Page",-1),u=[t];function a(n,e){return(0,r.wg)(),(0,r.iD)("div",s,u)}var i=c(89);const o={},h=(0,i.Z)(o,[["render",a]]);var l=h}}]);
+//# sourceMappingURL=about.77fa2dba.js.map
